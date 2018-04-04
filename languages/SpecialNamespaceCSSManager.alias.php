f9253cb7f2b524da39967a1c69e85113e25d221f
rvogel <vogel@hallowelt.biz>
2018-04-04 22:12:18 +0200

    Move to ".css" titles
    To enable proper syntax-highlighting and CodeEditor-support the naming
    convention is being changed.
    
    A update-maintenance script will rename existing pages
    (e.g. 'MediWiki:Help_css' -> 'MediaWiki:Help.css')
    
    Also a very simple manager special page is introduced to improve useability
    
    Change-Id: Ifc42638f8113a27ecab9e7b8fade1abab43d5018

---
+++ languages/SpecialNamespaceCSSManager.alias.php
@@ -0,0 +1,12 @@
+<?php
+$specialPageAliases = array();
+
+/** English */
+$specialPageAliases['en'] = array(
+	'NamespaceCSSManager' => array( 'NamespaceCSSManager', 'Namespace CSS Manager' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'NamespaceCSSManager' => array( 'NamespaceCSSManager', 'Namespace CSS Verwaltung' ),
+);
--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -2,40 +2,27 @@
 
 namespace BlueSpice\NamespaceCSS\Hook\BeforePageDisplay;
 
+use BlueSpice\NamespaceCSS\Helper;
+
 class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 	protected function skipProcessing() {
-		if( $this->getNamespace() < 0 ) {
-			return true;
-		}
-		if( !$nsName = \MWNamespace::getCanonicalName( $this->getNamespace() ) ) {
-			return true;
-		}
-		if( !$srcTitle = \Title::newFromText( $nsName.'_css', NS_MEDIAWIKI ) ) {
-			return true;
-		}
-		if( !$srcTitle->exists() ) {
+		$title = Helper::buildTitleFromNamespaceIndex(
+			$this->out->getTitle()->getNamespace()
+		);
+		if( !$title ) {
 			return true;
 		}
 		return false;
 	}
 
 	protected function doProcess() {
-		$srcTitle = \Title::newFromText(
-			\MWNamespace::getCanonicalName( $this->getNamespace() ).'_css',
-			NS_MEDIAWIKI
+		$title = Helper::buildTitleFromNamespaceIndex(
+			$this->out->getTitle()->getNamespace()
 		);
-		$this->out->addStyle( $srcTitle->getLocalUrl( [
+		$this->out->addStyle( $title->getLocalUrl( [
 			'action' => 'raw',
 			'ctype' => 'text/css'
 		]));
 		return true;
-	}
-
-	protected function getNamespace() {
-		$namespace = $this->out->getTitle()->getNamespace();
-		if( $this->out->getTitle()->isTalkPage() ) {
-			$namespace --;
-		}
-		return $namespace;
 	}
 }
