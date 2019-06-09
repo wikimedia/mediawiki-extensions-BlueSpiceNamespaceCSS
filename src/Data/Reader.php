7260f287d1528e25bca1b0cc953505fd0ae57625
pwirth <wirth@hallowelt.biz>
2019-06-09 08:43:01 +0200

    Remove exception rules for phpcs
    => Needs cherry-pick to REL1_31_dev and REL1_31
    
    Change-Id: Ifc82e706b256def3c09bb7fc2fb938f86c45ed72

--- src/Data/Reader.php
+++ src/Data/Reader.php
@@ -6,14 +6,27 @@ use BlueSpice\Services;
 
 class Reader extends \BlueSpice\Data\DatabaseReader {
 
+	/**
+	 *
+	 * @param \BlueSpice\Data\ReaderParams $params
+	 * @return PrimaryDataProvider
+	 */
 	protected function makePrimaryDataProvider( $params ) {
 		return new PrimaryDataProvider( $this->db, $this->context );
 	}
 
+	/**
+	 *
+	 * @return Schema
+	 */
 	public function getSchema() {
 		return new Schema();
 	}
 
+	/**
+	 *
+	 * @return SecondaryDataProvider
+	 */
 	public function makeSecondaryDataProvider() {
 		return new SecondaryDataProvider(
 			Services::getInstance()->getLinkRenderer()
