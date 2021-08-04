2db96e40f27a514decc747c8173202ce595c2d14
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 15:45:57 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: I323adc76df1d37672c44e7f810085d445201e5da

---
+++ src/GlobalActionsManager.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace BlueSpice\NamespaceCSS;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsManager extends RestrictedTextLink {
+
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-namespacecss';
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			'editinterface'
+		];
+		return $permissions;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'BlueSpiceNamespaceCSSManager' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-namespacecss-desc' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bluespicenamespacecssmanager' );
+	}
+
+	/**
+	 *
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bluespicenamespacecssmanager' );
+	}
+
+}
---
+++ src/HookHandler/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlueSpice\NamespaceCSS\HookHandler;
+
+use BlueSpice\NamespaceCSS\GlobalActionsManager;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'GlobalActionsManager',
+			[
+				'special-bluespice-namespacecss' => [
+					'factory' => static function () {
+						return new GlobalActionsManager();
+					}
+				]
+			]
+		);
+	}
+}
